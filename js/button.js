2e772d020f92a967fa62456920c303ca7a06f027
Jade <112060601+poisk-ls@users.noreply.github.com>
2023-04-18 22:54:41 +0800

    Add files via upload

---
+++ js/button.js
@@ -0,0 +1,9 @@
+function email() {
+        document.getElementById("email").innerHTML = "<strong>bsit.3s.buhayan.jadenelson@gmail.com</strong>"
+}
+
+function phone() {
+        document.getElementById("phone").innerHTML = "<strong>+63 9052877252</strong>"
+}
+
+/* My Social Account */
---
+++ js/side.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidenav1")
+    .style.width = "190px";
+}
+
+function closeNav() {
+    document.getElementById("sidenav1")
+    .style.width = "0px";
+}
